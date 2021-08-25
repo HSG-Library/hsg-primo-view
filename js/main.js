63126294699d2755c7e9f2392d2081d224cc0dbb
Jonas Furrer <jonas.furrer@gmail.com>
2021-08-25 10:59:18 +0200

    Disable locations filter module
    - The filter bar is now again hidden by default

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import { hsgExceptionHandlerModule } from './modules/hsg-exception-handler/hsg-e
 import { hsgAlmaViewitAfterModule } from './modules/prm-alma-viewit-after/hsg-alma-viewit-after.module';
 import { hsgJournalsAfterModule } from './modules/prm-journals-after/hsg-journals-after.module';
 import { hsgLocationItemsAfterModule } from './modules/prm-location-items-after/hsg-location-items-after.module';
-import { hsgLocationsAfterModule } from './modules/prm-locations-after/hsg-locations-after.module';
+//import { hsgLocationsAfterModule } from './modules/prm-locations-after/hsg-locations-after.module';
 import { hsgPersonalInfoAfterModule } from './modules/prm-personal-info-after/hsg-personal-info-after.module';
 import { hsgRequestAfterModule } from './modules/prm-request-after/hsg-request-after.module';
 import { hsgUserAreaExpandableAfterModule } from './modules/prm-user-area-expandable-after/hsg-user-ara-expandable-after.module';
@@ -18,7 +18,7 @@ app.requires.push(hsgHowovpAfterModule.name);
 app.requires.push(hsgAlmaViewitAfterModule.name);
 app.requires.push(hsgJournalsAfterModule.name);
 app.requires.push(hsgLocationItemsAfterModule.name);
-app.requires.push(hsgLocationsAfterModule.name);
+//app.requires.push(hsgLocationsAfterModule.name);
 app.requires.push(hsgPersonalInfoAfterModule.name);
 app.requires.push(hsgRequestAfterModule.name);
 app.requires.push(hsgUserAreaExpandableAfterModule.name);

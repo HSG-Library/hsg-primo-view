9f8ac65a7cc729963675878007e99a5e567d71ef
Jonas Furrer <jonas.furrer@gmail.com>
2021-09-08 14:35:28 +0200

    Disable reading room component

--- js/modules/prm-request-after/hsg-request-after.module.js
+++ js/modules/prm-request-after/hsg-request-after.module.js
@@ -1,6 +1,6 @@
 
 import { slspCourierInfoModule } from './slsp-courier-info/slsp-courier-info.module';
-import { hsgReadingRoomInfoModule } from './hsg-reading-room-info/hsg-reading-room-info.module';
+//import { hsgReadingRoomInfoModule } from './hsg-reading-room-info/hsg-reading-room-info.module';
 
 export const hsgRequestAfterModule = angular
 	.module('hsgRequestAfterModule', [])
@@ -8,9 +8,9 @@ export const hsgRequestAfterModule = angular
 		bindings: { parentCtrl: '<' },
 		template: `
 		<slsp-courier-info-component after-ctrl="$ctrl"></slsp-courier-info-component>
-		<hsg-reading-room-info-component after-ctrl="$ctrl"></hsg-reading-room-info-component>
 		`
+		// <hsg-reading-room-info-component after-ctrl="$ctrl"></hsg-reading-room-info-component>
 	});
 
 hsgRequestAfterModule.requires.push(slspCourierInfoModule.name);
-hsgRequestAfterModule.requires.push(hsgReadingRoomInfoModule.name);
+//hsgRequestAfterModule.requires.push(hsgReadingRoomInfoModule.name);

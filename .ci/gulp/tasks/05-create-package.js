7fb827253d6bafd0b5fe7edb47a4bc77d4c9f3b1
Jonas Furrer <jonas.furrer@gmail.com>
2021-11-18 17:20:22 +0100

    Fix directory structure of the package

--- .ci/gulp/tasks/05-create-package.js
+++ .ci/gulp/tasks/05-create-package.js
@@ -1,15 +1,26 @@
 'use strict';
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const { view } = require('../config.js');
 const config = require('../config.js');
 
-gulp.task('create-package', gulp.series('clean', 'select-view', 'custom-js','custom-css', function () {
+gulp.task('create-package', gulp.series('clean', 'select-view', 'custom-js', 'custom-css', function () {
     const code = config.view();
-    console.log('Creating package for : ('+code+'.zip)');
+    console.log('Creating package for : (' + code + '.zip)');
     console.log(code);
     console.log('\r\n');
-    console.log('............................................................................................................................................');
-    return gulp.src(['../','../html/**','../img/**','../css/custom1.css','../js/custom.js'], {base: '..'})
-        .pipe(zip(code+'.zip'))
+    console.log('.................................');
+    console.log('create view dir:', code);
+
+    const tempDir = 'temp/';
+    const viewDir = tempDir + code;
+    gulp.src('*.*', { read: false })
+        .pipe(gulp.dest(viewDir));
+
+    gulp.src(['../', '../html/**', '../img/**', '../css/custom1.css', '../js/custom.js'], { base: '..' })
+        .pipe(gulp.dest(viewDir));
+
+    return gulp.src(viewDir + '/**', { base: tempDir })
+        .pipe(zip(code + '.zip'))
         .pipe(gulp.dest('.'));
 }));

c9dbcea53807e970ee019cbba22ee93a94b62391
Jonas Furrer <jonas.furrer@gmail.com>
2021-11-18 12:57:29 +0100

    Disable BrowZine for prod release

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ import { hsgLocationItemsAfterModule } from './modules/prm-location-items-after/
 import { hsgLocationsAfterModule } from './modules/prm-locations-after/hsg-locations-after.module';
 import { hsgPersonalInfoAfterModule } from './modules/prm-personal-info-after/hsg-personal-info-after.module';
 import { hsgRequestAfterModule } from './modules/prm-request-after/hsg-request-after.module';
-import { hsgSearchResultAvailabilityLineAfterModule } from './modules/prm-search-result-availability-line-after/hsg-search-result-availabilty-line-after.module';
+//BrowZine not enabled yet // import { hsgSearchResultAvailabilityLineAfterModule } from './modules/prm-search-result-availability-line-after/hsg-search-result-availabilty-line-after.module';
 import { hsgStackMapAfterModule } from './modules/prm-stack-map-after/hsg-stack-map-after.module';
 import { hsgUserAreaExpandableAfterModule } from './modules/prm-user-area-expandable-after/hsg-user-ara-expandable-after.module';
 import { slspHttpInterceptRequestsModule } from './modules/slsp-http-intercept-requests/slsp-http-intercept-requests.module';
@@ -26,17 +26,18 @@ app.requires.push(hsgLocationItemsAfterModule.name);
 app.requires.push(hsgLocationsAfterModule.name);
 app.requires.push(hsgPersonalInfoAfterModule.name);
 app.requires.push(hsgRequestAfterModule.name);
-app.requires.push(hsgSearchResultAvailabilityLineAfterModule.name);
+//BrowZine not enabled yet // app.requires.push(hsgSearchResultAvailabilityLineAfterModule.name);
 app.requires.push(hsgStackMapAfterModule.name);
 app.requires.push(hsgUserAreaExpandableAfterModule.name);
 app.requires.push(slspHttpInterceptRequestsModule.name);
 
 app.run(function () { console.log('** app running **') });
 
+/* //BrowZine not enabled yet //
 // Begin BrowZine - Primo Integration (See: https://thirdiron.atlassian.net/wiki/spaces/BrowZineAPIDocs/pages/79200260/Ex+Libris+Primo+Integration)
 window.browzine = {
 	api: "https://public-api.thirdiron.com/public/v1/libraries/2827",
-	apiKey: "17316fec-6c1d-4f1f-b68a-20053d35dfb8",
+	apiKey: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
 
 	// **labels maintained and translated in hsg-browzine-integration module**
 	journalCoverImagesEnabled: true,
@@ -61,5 +62,5 @@ browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine
 document.head.appendChild(browzine.script);
 
 // **controller is implemented in hsg-browzine-integration module**
-
 // End BrowZine - Primo Integration
+*/

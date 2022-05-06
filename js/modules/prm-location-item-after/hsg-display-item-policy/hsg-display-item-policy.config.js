c180d5d4e79a2c858d7f00b3e50a0cb5e764fad4
Jonas Furrer <jonas.furrer@gmail.com>
2022-05-06 08:39:41 +0200

    Update translations for item-policy module

--- js/modules/prm-location-item-after/hsg-display-item-policy/hsg-display-item-policy.config.js
+++ js/modules/prm-location-item-after/hsg-display-item-policy/hsg-display-item-policy.config.js
@@ -1,16 +1,16 @@
 export const hsgDisplayItemPolicyConfig = {
 	labels: {
 		onSite: { // 63 and 68
-			de: 'Library use only',
-			en: 'Library use only'
+			de: 'On-site use only',
+			en: 'On-site use only'
 		},
 		unavailable: { // 67 
 			de: 'No use, no copy',
 			en: 'No use, no copy'
 		},
 		onRequest: { // 70
-			de: 'Use on request, contact the library',
-			en: 'Use on request, contact the library'
+			de: 'Use on request, contact the IFF library',
+			en: 'Use on request, contact the IFF library'
 		}
 	}
 }

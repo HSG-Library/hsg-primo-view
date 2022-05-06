33936e554091633308201d9052f02b13cb93b86e
Jonas Furrer <jonas.furrer@gmail.com>
2022-05-06 08:39:59 +0200

    Add missing dot at end of sentence

--- js/modules/prm-stand-alone-login-after/hsg-stand-alone-login-info/hsg-stand-alone-login-info.config.js
+++ js/modules/prm-stand-alone-login-after/hsg-stand-alone-login-info/hsg-stand-alone-login-info.config.js
@@ -5,8 +5,8 @@ export const hsgStandAloneLoginInfoConfig = {
 			en: '<b>HSGswisscovery</b> Login'
 		},
 		text: {
-			de: 'Hier stehen Ihnen einfache und erweiterte Suchfunktionen für Bücher, E-Books, Zeitschriften, Datenbanken und weitere Medien zur Verfügung. Melden Sie sich an um Ihr Konto, Ihre Ausleihen und Ihre Anfragen zu verwalten',
-			en: 'Here you can perform simple and advanced searches for books, e-books, journals, databases and other media. Login to manage your account, your loans and your requests'
+			de: 'Hier stehen Ihnen einfache und erweiterte Suchfunktionen für Bücher, E-Books, Zeitschriften, Datenbanken und weitere Medien zur Verfügung. Melden Sie sich an um Ihr Konto, Ihre Ausleihen und Ihre Anfragen zu verwalten.',
+			en: 'Here you can perform simple and advanced searches for books, e-books, journals, databases and other media. Login to manage your account, your loans and your requests.'
 		}
 	},
 }

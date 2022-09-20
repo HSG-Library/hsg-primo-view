3260f0e0460cd4999328a57fa109e73ee324e94b
Jonas Furrer <jonas.furrer@gmail.com>
2022-09-20 11:01:37 +0200

    Add module 'slsp-icon-label-other-members'

--- js/modules/prm-alma-other-members-after/hsg-alma-other-members-after.module.js
+++ js/modules/prm-alma-other-members-after/hsg-alma-other-members-after.module.js
@@ -1,10 +1,15 @@
 import { slspCollapseOtherInstModule } from './slsp-collapse-other-inst/slsp-collapse-other-inst.module'
+import { slspIconLabelOtherMembersModule } from './slsp-icon-label-other-members/slsp-icon-label-other-members.module'
 
 export const hsgAlmaOtherMembersAfterModule = angular
 	.module('hsgAlmaOtherMembersAfterModule', [])
 	.component('prmAlmaOtherMembersAfter', {
 		bindings: { parentCtrl: '<' },
-		template: `<slsp-collapse-other-inst-component after-ctrl="$ctrl"></slsp-collapse-other-inst-component>`
+		template: `
+		<slsp-collapse-other-inst-component after-ctrl="$ctrl"></slsp-collapse-other-inst-component>
+		<slsp-icon-label-other-members-component after-ctrl="$ctrl"></slsp-icon-label-other-members-component>
+		`
 	})
 
 hsgAlmaOtherMembersAfterModule.requires.push(slspCollapseOtherInstModule.name)
+hsgAlmaOtherMembersAfterModule.requires.push(slspIconLabelOtherMembersModule.name)

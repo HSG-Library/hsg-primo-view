f23dacd8736f3e7892a24aad69658f4d0fb8ade5
Jonas Furrer <jonas.furrer@gmail.com>
2022-09-21 09:08:33 +0200

    Add module 'slsp-rapido-digital-offer'

--- js/modules/prm-service-physical-best-offer-after/hsg-service-physical-best-offer-after.module.js
+++ js/modules/prm-service-physical-best-offer-after/hsg-service-physical-best-offer-after.module.js
@@ -1,10 +1,15 @@
 import { slspIconLabelRapidoModule } from './slsp-icon-label-rapido/slsp-icon-label-rapido.module'
+import { slspRapidoDigitalOfferModule } from './slsp-rapido-digital-offer/slsp-rapido-digital-offer.module'
 
 export const hsgServicePhysicalBestOfferAfterModule = angular
 	.module('hsgServicePhysicalBestOfferAfterModule', [])
 	.component('prmServicePhysicalBestOfferAfter', {
 		bindings: { parentCtrl: '<' },
-		template: `<slsp-icon-label-rapido-component after-ctrl="$ctrl"></slsp-icon-label-rapido-component>`,
+		template: `
+		<slsp-icon-label-rapido-component after-ctrl="$ctrl"></slsp-icon-label-rapido-component>
+		<slsp-rapido-rigital-offer-component after-ctrl="$ctrl"></slsp-rapido-rigital-offer-component>
+		`,
 	})
 
 hsgServicePhysicalBestOfferAfterModule.requires.push(slspIconLabelRapidoModule.name)
+hsgServicePhysicalBestOfferAfterModule.requires.push(slspRapidoDigitalOfferModule.name)
---
+++ js/modules/prm-service-physical-best-offer-after/slsp-rapido-digital-offer/slsp-rapido-digital-offer.module.js
@@ -0,0 +1,9 @@
+import { slspRapidoDigitalOfferController } from './slsp-rapido-digital-offer.controller'
+
+export const slspRapidoDigitalOfferModule = angular
+	.module('slspRapidoDigitalOfferModule', [])
+	.controller('slspRapidoDigitalOfferController', slspRapidoDigitalOfferController)
+	.component('slspRapidoDigitalOfferComponent', {
+		bindings: { afterCtrl: '<' },
+		controller: 'slspRapidoDigitalOfferController'
+	})

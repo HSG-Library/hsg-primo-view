1493d74276af629e3f8b8a461bab7cc47821e258
Jonas Furrer <jonas.furrer@gmail.com>
2022-10-12 08:22:07 +0200

    Update slsp-collapse-other-inst controller
    - According to SLSP sprint 7 update

--- js/modules/prm-alma-other-members-after/slsp-collapse-other-inst/slsp-collapse-other-inst.controller.js
+++ js/modules/prm-alma-other-members-after/slsp-collapse-other-inst/slsp-collapse-other-inst.controller.js
@@ -1,8 +1,25 @@
 export class slspCollapseOtherInstController {
-	constructor() { }
+
+	constructor($scope, $compile) {
+		this.$scope = $scope
+		this.$compile = $compile
+	}
 
 	$onInit() {
 		this.parentCtrl = this.afterCtrl.parentCtrl
-		this.parentCtrl.isCollapsed = true
+		if (this.parentCtrl.serviceMode !== 'howovp') {
+			this.parentCtrl.isCollapsed = true
+		}
+		else {
+			this.parentCtrl.isCollapsed = false
+			if (this.parentCtrl.availabilityType == 'P') {
+				this.noItemAtPickupLocation = noItemAtPickupLocation
+				let noItemAtPickupLocation = document.querySelectorAll(`prm-alma-other-members[ng-if="$ctrl.ifShowOther('P')"]`)
+				let htmlnoItemAtPickupLocation = `<div class="bar alert-bar zero-margin-bottom layout-align-center-center layout-row"><span class="center" translate="customized.alert.nolocations">​</span></div>`
+				angular.element(noItemAtPickupLocation).prepend(this.$compile(htmlnoItemAtPickupLocation)(this.$scope)).addClass('noItemAtPickupLocation')
+			}
+		}
 	}
 }
+
+slspCollapseOtherInstController.$inject = ['$scope', '$compile']

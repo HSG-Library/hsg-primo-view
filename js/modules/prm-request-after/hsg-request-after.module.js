0c7fd54a6a00b27a72e8bf3ed40e6bd369727691
Jonas Furrer <jonas.furrer@gmail.com>
2022-11-02 15:36:16 +0100

    Add 'slsp-alma-request' module

--- js/modules/prm-request-after/hsg-request-after.module.js
+++ js/modules/prm-request-after/hsg-request-after.module.js
@@ -1,4 +1,5 @@
 
+import { slspAlmaRequestModule } from './slsp-alma-request/slsp-alma-request.module'
 import { slspCourierInfoModule } from './slsp-courier-info/slsp-courier-info.module'
 //import { hsgReadingRoomInfoModule } from './hsg-reading-room-info/hsg-reading-room-info.module';
 
@@ -7,10 +8,12 @@ export const hsgRequestAfterModule = angular
 	.component('prmRequestAfter', {
 		bindings: { parentCtrl: '<' },
 		template: `
+		<slsp-alma-request-component after-ctrl="$ctrl"></slsp-alma-request-component>
 		<slsp-courier-info-component after-ctrl="$ctrl"></slsp-courier-info-component>
 		`
 		// <hsg-reading-room-info-component after-ctrl="$ctrl"></hsg-reading-room-info-component>
 	})
 
+hsgRequestAfterModule.requires.push(slspAlmaRequestModule.name)
 hsgRequestAfterModule.requires.push(slspCourierInfoModule.name)
 //hsgRequestAfterModule.requires.push(hsgReadingRoomInfoModule.name);
---
+++ js/modules/prm-request-after/slsp-alma-request/slsp-alma-request.module.js
@@ -0,0 +1,9 @@
+import { slspAlmaRequestController } from './slsp-alma-request.controller'
+
+export const slspAlmaRequestModule = angular
+	.module('slspAlmaRequestModule', [])
+	.controller('slspAlmaRequestController', slspAlmaRequestController)
+	.component('slspAlmaRequestComponent', {
+		bindings: { afterCtrl: '<' },
+		controller: 'slspAlmaRequestController'
+	})

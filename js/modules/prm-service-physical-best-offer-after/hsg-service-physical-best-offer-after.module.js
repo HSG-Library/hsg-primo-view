c40dc2d205f84086ddecba056b5c0008e5270bc6
Jonas Furrer <jonas.furrer@gmail.com>
2022-11-17 16:17:54 +0100

    Add 'eth-disable-request-button' module

--- js/modules/prm-service-physical-best-offer-after/hsg-service-physical-best-offer-after.module.js
+++ js/modules/prm-service-physical-best-offer-after/hsg-service-physical-best-offer-after.module.js
@@ -1,3 +1,4 @@
+import { ethDisableRequestButtonModule } from './eth-disable-request-button/eth-disable-request-button.module'
 import { slspIconLabelRapidoModule } from './slsp-icon-label-rapido/slsp-icon-label-rapido.module'
 import { slspRapidoDigitalOfferModule } from './slsp-rapido-digital-offer/slsp-rapido-digital-offer.module'
 
@@ -7,9 +8,11 @@ export const hsgServicePhysicalBestOfferAfterModule = angular
 		bindings: { parentCtrl: '<' },
 		template: `
 		<slsp-icon-label-rapido-component after-ctrl="$ctrl"></slsp-icon-label-rapido-component>
+		<eth-disable-request-button-component after-ctrl="$ctrl"></eth-disable-request-button-component>
 		<slsp-rapido-digital-offer-component after-ctrl="$ctrl"></slsp-rapido-digital-offer-component>
 		`,
 	})
 
+hsgServicePhysicalBestOfferAfterModule.requires.push(ethDisableRequestButtonModule.name)
 hsgServicePhysicalBestOfferAfterModule.requires.push(slspIconLabelRapidoModule.name)
 hsgServicePhysicalBestOfferAfterModule.requires.push(slspRapidoDigitalOfferModule.name)

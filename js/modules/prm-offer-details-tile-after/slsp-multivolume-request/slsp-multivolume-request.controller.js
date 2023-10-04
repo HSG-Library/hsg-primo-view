6a27bc05f227ce63f71be75767f29cafebdfcda4
Jonas Furrer <jonas.furrer@gmail.com>
2023-10-04 10:59:23 +0200

    Implement fix for disabled rapido order button
    - See: https://slsp.atlassian.net/servicedesk/customer/portal/1/SUPPORT-26219

--- js/modules/prm-offer-details-tile-after/slsp-multivolume-request/slsp-multivolume-request.controller.js
+++ js/modules/prm-offer-details-tile-after/slsp-multivolume-request/slsp-multivolume-request.controller.js
@@ -1,33 +1,38 @@
 export class slspMultivolumeRequestController {
 	constructor($scope) {
 		this.$scope = $scope
-		this.previousUnavailableResource = null
+		this.previousUnavailableVolume = null
+	}
+
+	$onInit() {
+		this.parentCtrl = this.afterCtrl.parentCtrl
 	}
 
 	$doCheck() {
-		this.parentCtrl = this.afterCtrl.parentCtrl
-		const currentUnavailableResource = this.parentCtrl.bestoffer.unavailableResource
-		if (currentUnavailableResource !== this.previousUnavailableResource) {
-			if (currentUnavailableResource !== false) {
+		const currentUnavailableVolume = this.parentCtrl.isNoOfferAfterRefine()
+		if (currentUnavailableVolume !== this.previousUnavailableVolume) {
+			if (currentUnavailableVolume == true) {
 				this.disableRequestButton()
 			} else {
 				this.enableRequestButton()
 			}
-			this.previousUnavailableResource = currentUnavailableResource
+
+			this.previousUnavailableVolume = currentUnavailableVolume
 		}
+
 	}
 
 	disableRequestButton() {
-		const requestButton = document.querySelector('button.button-with-icon.button-confirm.md-button.md-primoExplore-theme.md-ink-ripple')
+		const requestButton = angular.element(document.querySelector('button.button-with-icon.button-confirm.md-button.md-primoExplore-theme.md-ink-ripple'))
 		if (requestButton) {
-			requestButton.disabled = true
+			requestButton.attr('disabled', 'disabled')
 		}
 	}
 
 	enableRequestButton() {
-		const requestButton = document.querySelector('button.button-with-icon.button-confirm.md-button.md-primoExplore-theme.md-ink-ripple')
+		const requestButton = angular.element(document.querySelector('button.button-with-icon.button-confirm.md-button.md-primoExplore-theme.md-ink-ripple'))
 		if (requestButton) {
-			requestButton.disabled = false
+			requestButton.removeAttr('disabled')
 		}
 	}
 }

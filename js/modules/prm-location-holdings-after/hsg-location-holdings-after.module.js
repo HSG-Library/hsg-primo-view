a4795be46ec8428b5351026802c2bb952d43c8f8
Furrer, Jonas <jonas.furrer@unisg.ch>
2024-05-02 15:00:04 +0200

    update holding display info sprint 3/24

--- js/modules/prm-location-holdings-after/hsg-location-holdings-after.module.js
+++ js/modules/prm-location-holdings-after/hsg-location-holdings-after.module.js
@@ -1,10 +1,13 @@
+import { slspHoldingCollapseModule } from './slsp-holding-collapse/slsp-holding-collapse.module'
 import { slspIButtonModule } from './slsp-i-button/slsp-i-button.module'
 
 export const hsgLocationHoldingsAfterModule = angular
 	.module('hsgLocationHoldingsAfterModule', [])
 	.component('prmLocationHoldingsAfter', {
 		bindings: { parentCtrl: '<' },
-		template: `<slsp-i-button-component after-ctrl="$ctrl"></slsp-i-button-component>`
+		template: `<slsp-i-button-component after-ctrl="$ctrl"></slsp-i-button-component>
+		<slsp-holding-collapse-component after-ctrl="$ctrl"></slsp-holding-collapse-component>`
 	})
 
 hsgLocationHoldingsAfterModule.requires.push(slspIButtonModule.name)
+hsgLocationHoldingsAfterModule.requires.push(slspHoldingCollapseModule.name)

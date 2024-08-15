a03180f1714b2b046760f8cd034970cbd5371b36
Furrer, Jonas <jonas.furrer@unisg.ch>
2024-08-15 14:46:16 +0200

    remove duplicate/old module

--- js/modules/prm-request-after/bcu-fees-link/bcu-fees-link.module.js
+++
@@ -1,9 +0,0 @@
-import { bcuFeesLinkController } from './bcu-fees-link.controller'
-
-export const bcuFeesLinkModule = angular
-	.module('bcuFeesLinkModule', [])
-	.controller('bcuFeesLinkController', bcuFeesLinkController)
-	.component('bcuFeesLinkComponent', {
-		bindings: { afterCtrl: '<' },
-		controller: 'bcuFeesLinkController',
-	})

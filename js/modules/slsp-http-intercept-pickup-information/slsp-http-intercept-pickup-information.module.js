7ae5f8b7cbfe290c844e242ddb0723a2c25e11f5
Furrer, Jonas <jonas.furrer@unisg.ch>
2025-05-23 13:58:18 +0200

    add changes from sprint 4/25

--- js/modules/slsp-http-intercept-pickup-information/slsp-http-intercept-pickup-information.module.js
+++ js/modules/slsp-http-intercept-pickup-information/slsp-http-intercept-pickup-information.module.js
@@ -3,30 +3,30 @@ export const slspHttpInterceptPickupInformation = angular.module('slspHttpInterc
 		$httpProvider.interceptors.push(['$q', ($q) => {
 			return {
 				'request': (request) => {
-					return request
+					return request;
 				},
 				'requestError': (request) => {
-					return $q.reject(request)
+					return $q.reject(request);
 				},
 				'responseError': (response) => {
-					return $q.reject(response)
+					return $q.reject(response);
 				},
 				'response': (response) => {
 					try {
 						if (/primaws\/rest\/pub\/ngrs\/ngrsPickupInformationUrl/.test(response.config.url)) {
-							//get userGroup from JWT
-							let userGroup = angular.element(document.querySelector('primo-explore')).controller('primo-explore').userSessionManagerService.jwtUtilService.getDecodedToken().userGroup
+							//get userGroup from JWT 
+							let userGroup = angular.element(document.querySelector('primo-explore')).controller('primo-explore').userSessionManagerService.jwtUtilService.getDecodedToken().userGroup;
 
 							//list of user groups to change delivery for
-							if (['91', '92'].includes(userGroup)) {
-								response.data.personalDelivery = 'None'
+							if (['91', '92', '02'].includes(userGroup)) {
+								response.data.personalDelivery = 'None';
 							}
 						}
 					} catch (error) {
-						console.log(error)
+						console.log(error);
 					}
-					return response
+					return response;
 				}
-			}
-		}])
-	}])
+			};
+		}]);
+	}]);

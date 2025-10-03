0fa9f9fd589ef46a929f85f6e686e6e1273cc5a5
Furrer, Jonas <jonas.furrer@unisg.ch>
2025-10-03 15:16:46 +0200

    update power automate trigger urls

--- js/modules/prm-alma-viewit-after/hsg-report-broken-link/hsg-report-broken-link.config.js
+++ js/modules/prm-alma-viewit-after/hsg-report-broken-link/hsg-report-broken-link.config.js
@@ -1,5 +1,5 @@
 export const hsgReportBrokenLinkConfig = {
-	reportEndpoint: 'https://prod-132.westeurope.logic.azure.com:443/workflows/eb4b8ea670a54e838c514ac0a6a76cdb/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=XtWYbw3Qo-ul4TVMIhHw51ak9D84T9eJNdqEj1m-P0Q',
+	reportEndpoint: 'https://defaulta7262e591b564f5aa4126f07181f48.ee.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/eb4b8ea670a54e838c514ac0a6a76cdb/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RtR4rVv92A7q7xy4pnVOMP4n7J59Vm4tRb9bnvuYWZ4',
 	labels: {
 		linkText: {
 			de: 'Fehlerhaften Link melden',
--- js/modules/prm-explore-main/hsg-alert-message/hsg-alert-message.config.js
+++ js/modules/prm-explore-main/hsg-alert-message/hsg-alert-message.config.js
@@ -1,3 +1,3 @@
 export const hsgAlertMessageConfig = {
-	messageUrl: 'https://prod-102.westeurope.logic.azure.com:443/workflows/9eabdf07638d4af5b9608f6a7ba8eaae/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=op9aSvmJ_-GlOhHbN8sOsUOZ5CPC3h_-SMi4Tz07HO4'
-}
+	messageUrl: 'https://defaulta7262e591b564f5aa4126f07181f48.ee.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/9eabdf07638d4af5b9608f6a7ba8eaae/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MYJ7ynlpOsdIOTuincTy-hjlO1a6TgCAz1dNVsYHKQI'
+};

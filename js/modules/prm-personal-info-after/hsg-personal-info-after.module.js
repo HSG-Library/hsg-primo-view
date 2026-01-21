ceeed43db4f21d1e9d91dad196cc571b039669b2
Marina Frei <marina.frei@unisg.ch>
2026-01-21 10:34:12 +0100

    Sprint 12/25, access to invoice document

--- js/modules/prm-personal-info-after/hsg-personal-info-after.module.js
+++ js/modules/prm-personal-info-after/hsg-personal-info-after.module.js
@@ -1,10 +1,12 @@
 import { slspEditPersonalDetailsModule } from './slsp-edit-personal-details/slsp-edit-personal-details.module'
+import { slspInvoicesModule } from './slsp-invoices/slsp-invoices.module'
 
 export const hsgPersonalInfoAfterModule = angular
 	.module('hsgPersonalInfoAfterModule', [])
 	.component('prmPersonalInfoAfter', {
 		bindings: { parentCtrl: '<' },
-		template: `<slsp-edit-personal-details-component after-ctrl="$ctrl" id="SLSPeditPersonalDetails"></slsp-edit-personal-details-component>`
-	})
+		template: `<slsp-edit-personal-details-component after-ctrl="$ctrl" id="SLSPeditPersonalDetails"></slsp-edit-personal-details-component> <slsp-invoices-component after-ctrl="$ctrl"></slsp-invoices-component><slsp-personal-info-after parent-ctrl="$parent.$ctrl"></slsp-personal-info-after>`
+	});
 
 hsgPersonalInfoAfterModule.requires.push(slspEditPersonalDetailsModule.name)
+slspPersonalInfoAfterModule.requires.push(slspInvoicesModule.name);
